4cf3926262ffe516d51fa31fd1f0232cc28b6002
Umama-shaikh <160320031+Umama-shaikh@users.noreply.github.com>
2024-09-12 17:28:56 +0500

    Update script.ts

--- milestone01/script.ts
+++ milestone01/script.ts
@@ -1,12 +1,12 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+let acc = document.getElementsByClassName("accordion");
+let i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
 
     /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
+    let panel = this.nextElementSibling;
     if (panel.style.display === "none") {
       panel.style.display = "block";
     } else {
